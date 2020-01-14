77594b056958fd928ba47ee4efe089914574e66a
Elias Hachichou <elias.hachichou@adwisit.se>
2020-01-14 10:33:16 +0100

    Cleanup

--- Resources/CustomKeywords.py
+++ Resources/CustomKeywords.py
@@ -1,13 +1,7 @@
 import pyautogui
 import pyperclip
 pyautogui.FAILSAFE = False
-"""Example library in Robot Framework format.
 
-- Formatting with *bold* and _italic_.
-- URLs like http://example.com are turned to links.
-- Custom links like [http://robotframework.org|Robot Framework] are supported.
-- Linking to `My Keyword` works.
-"""
 
 def get_location(image):
     '''
